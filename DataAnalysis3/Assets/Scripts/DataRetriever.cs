ddfa414fa7d10b947b00f5d251edbb78a01f89a3
Jordi Espriu <37652360+LordUnicorn31@users.noreply.github.com>
2023-01-10 20:18:22 +0100

    Heatmap logic made + parsing of string data working (kind of)

--- DataAnalysis3/Assets/Scripts/DataRetriever.cs
+++ DataAnalysis3/Assets/Scripts/DataRetriever.cs
@@ -6,6 +6,7 @@ using UnityEngine.Analytics;
 public class DataRetriever : MonoBehaviour
 {
     private string data;
+    public Vector3 positions;
     void Start()
     {
         // Send a request to the PHP script
@@ -27,6 +28,7 @@ public class DataRetriever : MonoBehaviour
         {
             data = www.text;
             Debug.Log(data);
+            DataConverter(data);
         }
     }
 
@@ -37,6 +39,44 @@ public class DataRetriever : MonoBehaviour
 
         var result = Analytics.CustomEvent("data",state);
         Debug.Log("Analysis: " + result.ToString());
+    }
+
+    private void DataConverter(string dat)
+    {
+        var sStrings = dat.Split("\"");
+        List<int> ints = new List<int>();
+        List<Vector3> positions = new List<Vector3>();
+
+        foreach (string s in sStrings)
+        {
+            if(int.TryParse(s,out int val))
+            {
+                ints.Add(val);
+            }
+        }
+
+        //foreach (int i in ints)
+        //{
+        //    Debug.Log(i.ToString());
+
+        //}
+
+        for (int j = 1; j <= ints.Count; ++j)
+        {
+            Vector3 position = new Vector3((float)ints[j], (float)ints[j+1], (float)ints[j+2]);
+            if (j % 3 == 0 || j == 1)
+            {
+                positions.Add(position);
+                Debug.Log(position.ToString());
+            }
+        }
+
+        //float x = float.Parse(posStrings[0]);
+        //float y = float.Parse(posStrings[1]);
+        //float z = float.Parse(posStrings[2]);
+
+        //positions = new Vector3(x, y, z);
+        //Debug.Log(positions.ToString());
     }
 
 }
